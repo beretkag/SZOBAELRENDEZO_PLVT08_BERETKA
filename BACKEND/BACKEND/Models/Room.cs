fd95732a467f47aaa0ce6921cc2900b75a361e04
beretkag <beretkag@gmail.com>
2025-04-22 16:56:51 +0200

    bútor elhelyezés és eltávolítás

--- BACKEND/BACKEND/Models/Room.cs
+++ BACKEND/BACKEND/Models/Room.cs
@@ -13,6 +13,28 @@
                 Space[i] = new Furniture[width];
             }
         }
+
+        public void Place(Furniture furniture, int row, int col)
+        {
+            for (int i = 0; i < furniture.Height; i++)
+            {
+                for (int j = 0; j < furniture.Width; j++)
+                {
+                    Space[row + i][col + j] = furniture;
+                }
+            }
+        }
+
+        public void Remove(Furniture furniture, int row, int col)
+        {
+            for (int i = 0; i < furniture.Height; i++)
+            {
+                for (int j = 0; j < furniture.Width; j++)
+                {
+                    Space[row + i][col + j] = null;
+                }
+            }
+        }
     }
 
 
