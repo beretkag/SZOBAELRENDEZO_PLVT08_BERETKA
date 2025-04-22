5083477a719d2ce32f616839100c48b4ab5ee67f
beretkag <beretkag@gmail.com>
2025-04-22 17:01:42 +0200

    Bútor forgatása

--- BACKEND/BACKEND/Models/Furniture.cs
+++ BACKEND/BACKEND/Models/Furniture.cs
@@ -5,5 +5,15 @@
         public string Name { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
+
+        public Furniture CloneRotated()
+        {
+            return new Furniture
+            {
+                Name = this.Name,
+                Width = this.Height,
+                Height = this.Width
+            };
+        }
     }
 }
