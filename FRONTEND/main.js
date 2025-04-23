819413ddb3ab03a1b7ed167266ccd4435e300aee
beretkag <beretkag@gmail.com>
2025-04-23 13:42:49 +0200

    Szoba kirajzolása

--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -59,3 +59,77 @@ async function FurnishRoom() {
       DrawRoom(matrix)
   }
 }
+
+
+
+function DrawRoom(matrix) {
+  const table = document.getElementById('roomTarget')
+  table.innerHTML = "";
+  table.style.borderCollapse = "collapse";
+
+  let size = 50/parseInt(document.getElementById("roomWidth").value); 
+  console.log(size);
+   
+  
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      if (!matrix[row][col]) {
+        matrix[row][col] = {
+          width: 1,
+          height: 1,
+          name: "",
+          drawn: false
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    let row = document.createElement("tr");
+    row.style.height = `${size}vw`
+    table.appendChild(row);
+
+    for (let j = 0; j < matrix[i].length; j++) {
+      let furniture = matrix[i][j];
+      
+      if (!furniture.drawn){
+        let td = document.createElement("td");
+        tdSize = GetFurnitureSize(matrix, i, j);
+        td.style.width = `${tdSize.width * size}vw`;
+        td.style.maxWidth = `${tdSize.width * size}vw`;
+        td.style.height = `${tdSize.height * size}vw`;
+        td.style.maxHeight = `${tdSize.height * size}vw`;
+        td.colSpan = `${tdSize.width}`
+        td.rowSpan = `${tdSize.height}`
+        td.innerHTML = matrix[i][j].name
+        signDrawnCells(matrix, tdSize, i, j);
+        if (furniture.name != "") {
+          td.classList.add("filledCell")
+        }
+        row.appendChild(td);
+      }
+    }
+  }
+}
+
+function GetFurnitureSize(matrix, row, col){
+  let name = matrix[row][col].name
+  let size = {width: 1, height: 1}
+  while (row + size.height < matrix.length && matrix[row + size.height][col].name == name && !matrix[row + size.height][col].drawn) {
+    size.height += 1;
+  }
+  while (col + size.width < matrix[row].length && matrix[row][col + size.width].name == name && !matrix[row][col + size.width].drawn) {
+    size.width += 1;
+  }
+  console.log(size);
+  
+  return size;
+}
+
+function signDrawnCells(matrix, tdSize, row, col){
+  for (let i = 0; i < tdSize.height; i++) {
+    for (let j = 0; j < tdSize.width; j++) {
+      matrix[row + i][col + j].drawn = true;
+    }
+  }
+}
